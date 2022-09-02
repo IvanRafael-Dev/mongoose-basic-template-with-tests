61ee6b3e6bbea2a8871a257409b14db442ac58eb
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-02 14:32:53 -0300

    test: ensure LensModel #readOne returns an error in an invalid _id is provided

--- src/tests/unit/models/lens.spec.ts
+++ src/tests/unit/models/lens.spec.ts
@@ -1,8 +1,11 @@
-import { expect } from "chai"
+import chai, { expect } from "chai"
+import chaiAsPromised from 'chai-as-promised'
 import { Model, Types } from "mongoose"
 import * as sinon from 'sinon'
 import ILens from "../../../models/interfaces/ILens"
 import Lens from "../../../models/Lens"
+
+chai.use(chaiAsPromised)
 
 const lensRequestMock: ILens = {
   antiGlare: true,
@@ -21,6 +24,7 @@ describe('Lens Model', () => {
   const lensModel = new Lens()
   beforeEach(() => {
     sinon.stub(Model, 'create').resolves(lensMockWithId)
+    sinon.stub(Model, 'findOne').resolves(lensMockWithId)
   })
   afterEach(() => {
     sinon.restore()
@@ -31,6 +35,22 @@ describe('Lens Model', () => {
       it('should return the created lens with related "_id"', async () => {
         const lens = await lensModel.create(lensRequestMock)
         expect(lens).to.be.equal(lensMockWithId)
+      })
+    })
+  })
+
+  describe('#readOne', () => {
+    describe('when the lens if found', () => {
+      it('should return the found lens with related "_id"', async () => {
+        const lens = await lensModel.readOne(_id)
+        expect(lens).to.be.equal(lensMockWithId)
+      })
+    })
+
+    describe('when an invalid "_id" is provided', () => {
+      it('should return an error', () => {
+        return expect(lensModel.readOne('invalid_id'))
+          .to.be.rejectedWith(Error, 'InvalidMongoId')
       })
     })
   })
