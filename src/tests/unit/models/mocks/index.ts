8510040a8af8210cb29fa4390fc79ff9ae1558be
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-02 15:29:34 -0300

    refactor: change typos approach

--- src/tests/unit/models/mocks/index.ts
+++ src/tests/unit/models/mocks/index.ts
@@ -2,6 +2,12 @@ import { Types } from "mongoose"
 import IFrame from "../../../../models/interfaces/IFrame"
 import ILens from "../../../../models/interfaces/ILens"
 
+// types
+type Indexable = { _id: string }
+type IModelReturns<T> = T & Indexable
+type IModelReturnsArray<T> = T[] & Indexable[]
+
+// request mocks
 const lensRequestMock: ILens = {
   antiGlare: true,
   blueLightFilter: true,
@@ -13,22 +19,43 @@ const frameRequestMock: IFrame = {
   material: 'wood'
 }
 
+// generate _id
 const _id = new Types.ObjectId() as unknown as string
 
-const lensMockWithId: ILens & { _id: string } = {
+// model retuns 
+
+const lensMockWithId: IModelReturns<ILens> = {
   _id,
   ...lensRequestMock
 }
 
-const frameMockWithId: IFrame & { _id: string } = {
+const frameMockWithId: IModelReturns<IFrame> = {
   _id,
   ...frameRequestMock
 }
 
+
+
+const lensMockArrayWithId: IModelReturnsArray<ILens> = [
+  {
+    _id,
+    ...lensRequestMock
+  }
+]
+
+const frameMockArrayWithId: IModelReturnsArray<IFrame> = [
+  {
+    _id,
+  ...frameRequestMock
+  }
+]
+
 export {
   lensRequestMock,
   frameRequestMock,
+  _id,
   frameMockWithId,
   lensMockWithId,
-  _id,
+  lensMockArrayWithId,
+  frameMockArrayWithId
 }
