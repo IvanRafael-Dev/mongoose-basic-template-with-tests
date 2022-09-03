1d456678a617414c0518817db18187b5163a0324
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-03 01:00:37 -0300

    test: ensure FrameService throws an Error if no frame is found

--- src/tests/unit/services/frame.spec.ts
+++ src/tests/unit/services/frame.spec.ts
@@ -15,13 +15,16 @@ describe('Frame Services', () => {
 
   before(() => {
     sinon.stub(frameModel, 'create').resolves(mock.frameMockWithId)
+    sinon.stub(frameModel, 'readOne')
+      .onCall(0).resolves(null)
+      .onCall(1).resolves(mock.frameMockWithId)
   })
 
   after(() => sinon.restore())
 
   describe('#create', () => {
     describe('when the frame request is invalid', () => {
-      it ('should throw an ZodError', async () => {
+      it ('should throw an ZodError', () => {
         return expect(frameService.create({} as IFrame))
           .to.be.rejectedWith(ZodError)        
       })
@@ -29,6 +32,22 @@ describe('Frame Services', () => {
     describe('when a frame is created with success', () => {
       it('should return the new frame obj', async () => {
         const frame = await frameService.create(mock.frameRequestMock)
+        expect(frame).to.be.equal(mock.frameMockWithId)
+      })
+    })
+  })
+
+  describe('#readOne', () => {
+    describe('if the frame is not found', () => {
+      it('should throw an error', () => {
+        return expect(frameService.readOne('not_found_id'))
+          .to.be.rejectedWith(Error, 'EntityNotFound')
+      })
+    })
+
+    describe('when the frame is found', () => {
+      it('should return the frame', async () => {
+        const frame = await frameService.readOne(mock._id)
         expect(frame).to.be.equal(mock.frameMockWithId)
       })
     })
