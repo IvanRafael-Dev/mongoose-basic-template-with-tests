340f5dbcbafd047c8c8ac74a940020c8d252bec1
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-04 00:14:13 -0300

    feat: add basic ErrorCatalog

--- src/erros/catalog.ts
+++ src/erros/catalog.ts
@@ -3,4 +3,24 @@ export enum ErrorTypes {
   InvalidMongoId = 'InvalidMongoId',
 }
 
+type ErrorResponseObject = {
+  message: string
+  statusCode: number
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject
+};
+
+export const errorCatalog: ErrorCatalog = {
+  EntityNotFound: {
+    message: 'Entity not found',
+    statusCode: 404,
+  },
+  InvalidMongoId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    statusCode: 400,
+  },
+};
+
 export default { teste: 'oi' };
