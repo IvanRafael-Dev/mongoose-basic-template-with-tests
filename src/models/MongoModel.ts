31800d5cc9362ff32ae55186bc6990981336d743
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-04 00:14:47 -0300

    feat: add InvalidMongoId Error to MongoModel

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,5 @@
 import { isValidObjectId, Model } from 'mongoose';
+import { ErrorTypes } from '../erros/catalog';
 import IModel from './interfaces/IModel';
 
 export default abstract class MongoModel<T> implements IModel<T> {
@@ -17,14 +18,14 @@ export default abstract class MongoModel<T> implements IModel<T> {
 
   async readOne(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) {
-      throw new Error('InvalidMongoId');
+      throw new Error(ErrorTypes.InvalidMongoId);
     }
     return this._model.findOne({ _id });
   }
 
   async destroy(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) {
-      throw new Error('InvalidMongoId');
+      throw new Error(ErrorTypes.InvalidMongoId);
     }
     return this._model.findByIdAndDelete({ _id });
   }
